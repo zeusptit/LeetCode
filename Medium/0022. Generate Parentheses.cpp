d2b7f5d4b09aa85c71cbda7f869cb0246cc5110b
Trần Phú Cường <cuongtranphu1@gmail.com>
2023-01-14 08:02:16 +0700

    Add files via upload

---
+++ Medium/0022. Generate Parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string s;
+    int check(string s){
+        stack<int> st; int k = 0;
+        for(char x : s){
+            if(x == '('){
+                st.push(x);
+                k++;
+            }
+            else{
+                if(!st.empty())st.pop();
+            }
+        }
+        return st.size() == 0 && k == s.size() / 2;
+    }
+    vector<string> ans; int cnt = 0;
+    void Try(int k, string s){
+        for(int j = 40; j <= 41; j++){
+            s += (char)j;
+            if(j == 40)cnt++;
+            if(s.size() == k){
+                if(check(s))
+                    ans.push_back(s);
+            }
+            else 
+                Try(k, s);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        Try(n * 2, "");
+        return ans;
+    }
+};
